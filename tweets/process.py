78b923a309628ee73df89f3b3006e00ca1da9ed0
Oziel Alves <ozielalves7@gmail.com>
2018-10-23 19:42:49 -0300

    Tweets fix

--- tweets/process.py
+++ tweets/process.py
@@ -1,20 +1,17 @@
 from contextlib import redirect_stdout
 import re
 
-reUrls = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-reUser = r'@\w+'
+match = re.compile(r'RT (@[A-Za-z0-9_]*).*(https:\/\/[t|.A-Za-z0-9/]+)')
 fake_newers = []
 
 with open("tweets.in", encoding="utf-8") as f:
     tweets = [line.strip() for line in f.readlines()]
-    rtts = [ i for i in tweets if i[:4] == 'RT @' ]
-    for i in rtts:
-        Url = re.findall(reUrls, i)
-        if Url:
-            User = re.findall(reUser, i)
-            fake_newers.append([User[0], Url[0]])    
+    for i in tweets:
+        matches = re.findall(match, i)
+        if matches:
+            fake_newers.append([matches[0][0], matches[0][1]])
     
-with open("tweets.out", "w", encoding="utf-8") as f:
-    with redirect_stdout(f):
+    with open("tweets.out", "w", encoding="utf-8") as f:
+        with redirect_stdout(f):
             for i in sorted(fake_newers):
-                print('\t'.join(i))
+                print('\t'.join(i))  
